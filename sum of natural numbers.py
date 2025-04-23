8a35b0781b3ef7af4c449d7d5280e3df8f12e042
Sufi <sufiansonic@gmail.com>
2025-04-23 20:00:14 +0900

    first commit

---
+++ sum of natural numbers.py
@@ -0,0 +1,11 @@
+number = int(input("Enter the natural number : "))
+i = 1
+sum = 0
+
+while (i<=number):
+    sum = sum + i
+    i = i + 1
+    
+print ("The sum of the natural numbers is " + str(sum))    
+
+
