93047b058bcd1ab9653b28dd605d4143b3aa9d71
Sufi <sufiansonic@gmail.com>
2025-04-26 16:07:55 +0900

    huhhu

---
+++ power calculator.py
@@ -0,0 +1,16 @@
+power = int(input("Enter the power : "))
+base = int(input("Enter the base : "))
+answer = 1
+for i in range(1, power + 1):
+    
+    answer = answer * base
+print("The power of the number is : ", answer)
+    
+
+    
+
+
+
+
+
+
